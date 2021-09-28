912315c933c78e9264f13264f5d40b108c152f72
Eugen Morozov <meugenom@gmail.com>
2021-09-28 19:40:49 +0200

    small changes

--- src/main/java/dev/eugenem/leetcode/RotateImage/RotateImage.java
+++ src/main/java/dev/eugenem/leetcode/RotateImage/RotateImage.java
@@ -1,9 +1,9 @@
 package dev.eugenem.leetcode.RotateImage;
 
 public class RotateImage {
-    
+
     public void rotate(int[][] matrix) {
-        
+
         //transpose
         for(int i = 0; i < matrix.length; i++) {
             for(int j = i; j < matrix[i].length; j++) {
@@ -12,7 +12,7 @@ public class RotateImage {
                 matrix[i][j] = tmp;
             }
         }
-        
+
         //reflect
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length / 2; j++) {
@@ -28,5 +28,5 @@ public class RotateImage {
         int[][] matrix = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
         ri.rotate(matrix);
         //expected : [[7,4,1],[8,5,2],[9,6,3]]
-    }    
+    }
 }
