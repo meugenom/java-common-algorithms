6ff82b433745cd77288e6082e812ecfb41d45868
Eugen Morozov <meugenom@gmail.com>
2022-07-15 21:20:03 +0200

    added solved solution

--- README.md
+++ README.md
@@ -178,6 +178,8 @@ Algorithm (operation)| Level |  Done  |  Big ⭕️ Notation | Tests passed
 ➿ |➿ |➿ |➿
 **_Binary Tree_** | 🟢 easy | Java |[ [617. Merge Two Binary Trees](src/main/java/com/meugenom/leetcode/MergeTwoBinaryTrees/MergeTwoBinaryTrees.java)
 **_Binary Tree_** | 🟡 medium | Java |[102. Binary Tree Level Order Traversal](src/main/java/com/meugenom/leetcode/BinaryTreeLevelOrderTraversal/BinaryTreeLevelOrderTraversal.java)
+**_Binary Tree_** | 🟡 medium | Java |[105. Construct Binary Tree from Preorder and Inorder Traversal](src/main/java/com/meugenom/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal/ConstructBinaryTreeFromPreorderAndInorderTraversal.java)
+ConstructBinaryTreeFromPreorderAndInorderTraversal.java
 **_Binary Tree_** | 🟡 medium | Java | [116. Populating Next Right Pointers in Each Node](src/main/java/com/meugenom/leetcode/PopulatingNextRightPointersEachNode/PopulatingNextRightPointersEachNode.java)
 **_Binary Tree_** | 🟡 medium | Java |[199. Binary Tree Right Side View](src/main/java/com/meugenom/leetcode/BinaryTreeRightSideView/BinaryTreeRightSideView.java)
 ➿ |➿ |➿ |➿
---
+++ src/main/java/com/meugenom/leetcode/ConstructBinaryTreeFromPreorderAndInorderTraversal/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,78 @@
+package com.meugenom.leetcode.ConstructBinaryTreeFromPreorderAndInorderTraversal;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+	public static int index;
+
+	public static Map<Integer, Integer> inorderIndexMap;
+
+	public static TreeNode buildTree(int[] preorder, int[] inorder) {
+
+		index = 0;
+
+		// build a hashmap to store value -> its index relations
+		inorderIndexMap = new HashMap<>();
+
+		for (int i = 0; i < inorder.length; i++) {
+
+			inorderIndexMap.put(inorder[i], i);
+
+		}
+
+		// for (int i = 0; i < inorder.length; i++) {
+		// System.out.println(inorderIndexMap.toString());
+		// }
+
+		return helper(preorder, 0, preorder.length - 1);
+	}
+
+	public static TreeNode helper(int[] preorder, int left, int right) {
+
+		if (left > right)
+			return null;
+
+		int val = preorder[index++];
+		TreeNode parent = new TreeNode(val);
+
+		// build left and right subtree
+		// preorder with left subtree
+		parent.left = helper(preorder, left, inorderIndexMap.get(val) - 1);
+
+		// inorder with right subtree
+		parent.right = helper(preorder, inorderIndexMap.get(val) + 1, right);
+
+		return parent;
+	}
+
+	public static void main(String[] args) {
+
+		int[] preorder = new int[] { 3, 9, 20, 15, 7 };
+		int[] inorder = new int[] { 9, 3, 15, 20, 7 };
+
+		System.out.println(buildTree(preorder, inorder));
+
+	}
+
+}
