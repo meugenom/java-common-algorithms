8e12abda4312ee99896bfea449d7114a9bca7350
Eugen Morozov <meugenom@gmail.com>
2022-07-30 09:50:57 +0200

    added solved problem from leetcode

--- src/main/java/com/meugenom/leetcode/LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.java
+++ src/main/java/com/meugenom/leetcode/LetterCombinationsOfPhoneNumber/LetterCombinationsOfPhoneNumber.java
@@ -1,62 +1,54 @@
 package com.meugenom.leetcode.LetterCombinationsOfPhoneNumber;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
-
-
-
 
 public class LetterCombinationsOfPhoneNumber {
-	
-	private static Hashtable<String, String[]> ht = new Hashtable<>();
-	
+
+	private static Hashtable<Integer, String[]> ht;
+	private static List<String> letters;
+
 	public static List<String> letterCombinations(String digits) {
 
-		ht.put("2", new String[]{"a", "b", "c"});
-		ht.put("3", new String[]{"d", "e", "f"});
-		ht.put("4", new String[]{"g", "h", "i"});
-		ht.put("5", new String[]{"j", "k", "l"});
-		ht.put("6", new String[]{"m", "n", "o"});
-		ht.put("7", new String[]{"p", "q", "r"});
-		ht.put("8", new String[]{"t", "u", "v"});
-		ht.put("9", new String[]{"w", "x", "y", "z"});
-        
-		List<String> letters = new ArrayList<String>();
-		//String result = "";
-		
-		//int step = 0;
-		//while(step < digits.length()){
-			
-			//System.out.println(ht.get(String.valueOf(digits.charAt(step)))[0]);
-			//letters
+		letters = new ArrayList<String>();
+		if (digits.length() == 0)
+			return letters;
 
+		ht = new Hashtable<>();
 
-		//	step++;
-		//}
+		ht.put(2, new String[] { "a", "b", "c" });
+		ht.put(3, new String[] { "d", "e", "f" });
+		ht.put(4, new String[] { "g", "h", "i" });
+		ht.put(5, new String[] { "j", "k", "l" });
+		ht.put(6, new String[] { "m", "n", "o" });
+		ht.put(7, new String[] { "p", "q", "r", "s" });
+		ht.put(8, new String[] { "t", "u", "v" });
+		ht.put(9, new String[] { "w", "x", "y", "z" });
+
+		helper(digits, 0, "");
 
 		return letters;
-    }
-
-	public static List<String> helper(List<String> result, String digits, int index){
-
-
-		String[] current = ht.get(digits.charAt(index));
-
-		int step = 0;
-		while(step < current.length){
-			//result = 
-			step++;
-		}
-
-		return result;
 	}
 
+	public static void helper(String digits, int index, String tmp) {
+
+		if (tmp.length() == digits.length()) {
+			letters.add(tmp);
+			return;
+		}
+
+		int val = Integer.parseInt(String.valueOf(digits.charAt(index)));
+		String[] s = ht.get(val);
+		for (int i = 0; i < s.length; i++) {
+			tmp = tmp + s[i];
+			helper(digits, index + 1, tmp);
+			tmp = tmp.substring(0, tmp.length() - 1);
+		}
+	}
 
 	public static void main(String[] args) {
-		String digits = "23";
+		String digits = "237";
 		List<String> res = letterCombinations(digits);
 		System.out.println(res.toString());
 
