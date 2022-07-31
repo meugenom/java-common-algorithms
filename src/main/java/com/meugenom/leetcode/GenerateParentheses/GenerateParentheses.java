c35d9ff199a672e99f2e6bc6160e29f0985b58cb
Eugen Morozov <meugenom@gmail.com>
2022-07-31 10:21:50 +0200

    added solved issue from leetcode

---
+++ src/main/java/com/meugenom/leetcode/GenerateParentheses/GenerateParentheses.java
@@ -0,0 +1,42 @@
+package com.meugenom.leetcode.GenerateParentheses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateParentheses {
+
+	private static List<String> res;
+
+	public static List<String> generateParenthesis(int n) {
+
+		res = new ArrayList<>();
+		String str = "";
+		int open = 0;
+		int close = 0;
+		backtrack(str, open, close, n);
+		return res;
+	}
+
+	public static void backtrack(String str, int open, int close, int n) {
+
+		if (str.length() == 2 * n) {
+
+			res.add(str);
+			return;
+		}
+
+		if (open < n) {
+			backtrack(str + "(", open + 1, close, n);
+		}
+
+		if (close < open) {
+			backtrack(str + ")", open, close + 1, n);
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 4;
+		System.out.println(generateParenthesis(n).toString());
+	}
+
+}
