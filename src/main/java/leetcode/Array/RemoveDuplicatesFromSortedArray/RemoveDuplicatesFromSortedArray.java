8ec6f502ae15dc50a828343a3d9a62f5557f2438
Eugen Morozov <meugenom@gmail.com>
2022-08-21 16:25:53 +0200

    moved array skills to the dir Array and fixed readme links

---
+++ src/main/java/leetcode/Array/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,33 @@
+package leetcode.Array.RemoveDuplicatesFromSortedArray;
+
+public class RemoveDuplicatesFromSortedArray {
+
+	public static int removeDuplicates(int[] nums) {
+
+		int count = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+
+			if (i == 0 || nums[i] != nums[i - 1]) {
+
+				nums[count] = nums[i];
+				count++;
+			}
+		}
+
+		for (int i = 0; i < nums.length; i++) {
+
+			System.out.println("nums[" + i + "] = " + nums[i]);
+
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		// int[] nums = new int[]{ 1, 1, 2};
+		int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		System.out.println(removeDuplicates(nums));
+	}
+
+}
