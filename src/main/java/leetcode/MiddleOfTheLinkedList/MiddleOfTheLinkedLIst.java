fb93ed2d813a135ef44fcc19eeccb6a19411d420
Eugen Morozov <meugenom@gmail.com>
2022-08-29 10:24:57 +0200

    rewriting of the code solution

--- src/main/java/leetcode/MiddleOfTheLinkedList/MiddleOfTheLinkedLIst.java
+++ src/main/java/leetcode/MiddleOfTheLinkedList/MiddleOfTheLinkedLIst.java
@@ -1,8 +1,26 @@
 package leetcode.MiddleOfTheLinkedList;
 
-/**
- * Definition for singly-linked list.
+/***
+ * Given the head of a singly linked list, return the middle node of the linked
+ * list.
+ * If there are two middle nodes, return the second middle node.
+ * 
+ * Example 1:
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * 
+ * Example 2:
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * 
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we
+ * return the second one.
+ * 
+ * T.C.: O(N);
+ * S.C.: O(N).
  */
+
 class ListNode {
 	int val;
 	ListNode next;
@@ -28,42 +46,56 @@ class ListNode {
 class MiddleOfTheLinkedList {
 
 	public static ListNode middleNode(ListNode head) {
+		
+		ListNode slow = head;
+		ListNode fast = head;
 
-		int length = length(head);
-		// System.out.println("lenght : " + length);
-
-		ListNode curr = head;
-		int count = 0;
-
-		while (curr != null) {
-
-			if (count == length / 2) {
-				System.out.println(curr.toString());
-				return curr;
-			}
-
-			count++;
-			curr = curr.next;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
 		}
-
-		return head;
+		return slow;
 	}
-
-	public static int length(ListNode head) {
-
-		if (head == null) {
-			return 0;
-		}
-
-		ListNode curr = head;
-		int count = 0;
-
-		while (curr != null) {
-			count++;
-			curr = curr.next;
-		}
-		return count;
-	}
+	//old solution
+	/*
+	 * public static ListNode middleNode(ListNode head) {
+	 * 
+	 * int length = length(head);
+	 * // System.out.println("lenght : " + length);
+	 * 
+	 * ListNode curr = head;
+	 * int count = 0;
+	 * 
+	 * while (curr != null) {
+	 * 
+	 * if (count == length / 2) {
+	 * //System.out.println(curr.toString());
+	 * return curr;
+	 * }
+	 * 
+	 * count++;
+	 * curr = curr.next;
+	 * }
+	 * 
+	 * return head;
+	 * }
+	 * 
+	 * public static int length(ListNode head) {
+	 * 
+	 * if (head == null) {
+	 * return 0;
+	 * }
+	 * 
+	 * ListNode curr = head;
+	 * int count = 0;
+	 * 
+	 * while (curr != null) {
+	 * count++;
+	 * curr = curr.next;
+	 * }
+	 * return count;
+	 * }
+	 */
 
 	public static void main(String[] args) {
 
@@ -85,7 +117,6 @@ class MiddleOfTheLinkedList {
 		a4.next = a5;
 		a5.next = null;
 
-		MiddleOfTheLinkedList s = new MiddleOfTheLinkedList();
-		System.out.println(s.middleNode(a1).toString());
+		System.out.println(middleNode(a1).toString());
 	}
 }
